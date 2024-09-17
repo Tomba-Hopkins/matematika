f36bd2fd7c61d53bdef6f9b86f8b2b28369f3a6c
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-09-17 08:32:47 +0700

    msg: styling

---
+++ views/app.js
@@ -0,0 +1,11 @@
+const uKnow = document.querySelector('.suku-diketahui')
+const a = document.querySelector('.nilai-suku-diketahui')
+const b = document.querySelector('.beda')
+const uN = document.querySelector('.suku-n')
+
+
+const rumusSequences = (suku, nilaiSuku, beda, sukuN) => {
+    if(suku == 1) {
+        return 
+    }
+}
