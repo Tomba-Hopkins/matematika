01b054d4791b6ce78673c34290bd542da76191d1
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-09-17 08:48:21 +0700

    scripting: js

--- views/app.js
+++ views/app.js
@@ -1,11 +1,26 @@
-const uKnow = document.querySelector('.suku-diketahui')
-const a = document.querySelector('.nilai-suku-diketahui')
-const b = document.querySelector('.beda')
-const uN = document.querySelector('.suku-n')
+const uKnow = document.querySelector('.suku-diketahui').value
+const a = document.querySelector('.nilai-suku-diketahui').value
+const b = document.querySelector('.beda').value
+const uN = document.querySelector('.suku-n').value
+
+
+const result = document.querySelector('.result')
+const RBtn = document.querySelector('.rButton')
 
 
 const rumusSequences = (suku, nilaiSuku, beda, sukuN) => {
     if(suku == 1) {
-        return 
+        return  nilaiSuku + (sukuN - 1) * beda
     }
 }
+
+
+
+RBtn.addEventListener('click', () => {
+    result.innerHTML = rumusSequences(uKnow, a, b, uN)
+    console.log(rumusSequences(uKnow, a, b, uN))
+    console.log(Number(uKnow))
+    console.log(Number(a))
+    console.log(Number(b))
+    console.log(uN)
+})
